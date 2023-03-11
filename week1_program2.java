2da58a63fc6ffde0ee617740d2b3f3a287b250f2
Raj-kumarpatidar <116540962+Raj-kumarpatidar@users.noreply.github.com>
2023-03-11 11:44:18 +0530

    Add files via upload
    Program 2: Write a program to display the message “This is just a test”. Pass these 5 strings as arguments in the main function while executing the program through cmd.

---
+++ week1_program2.java
@@ -0,0 +1,14 @@
+/**
+ * week1_program2
+ */
+public class week1_program2 {
+
+    public static void main(String args[]) {
+       for(int i=0;i<args.length;i++)
+       {
+        System.out.println(args[i]);
+       }
+        
+    }
+
+}
