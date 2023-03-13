e39b13f0221d677a0f484a051d97807bab8f00c9
Raj-kumarpatidar <116540962+Raj-kumarpatidar@users.noreply.github.com>
2023-03-13 13:08:44 +0530

    Add files via upload
    Program 2: WAP make a class A in that class declare function add perform addition through main.

---
+++ week2_program2.java
@@ -0,0 +1,25 @@
+ class A
+{
+   
+    static void add(int a,int b)
+    {
+         System.out.println(a+b);
+    }
+    public static void  main(String args[])
+
+    {
+        add(5,6);
+    }
+}
+
+ public  class week2_program2  
+{
+    public static void main(String args[])
+    {
+       // A obj=new A();
+       // obj.add(5,6);
+        //System.out.println(obj.add(5,2));
+    }
+
+   
+}
